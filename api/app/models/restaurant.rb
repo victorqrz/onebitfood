2582e4613bea3d3115dd45dafca0f3c2513fea9b
Victor Queiroz <victoorqrz@gmail.com>
2021-12-06 23:32:17 -0300

    configuring active storage for images

--- api/app/models/restaurant.rb
+++ api/app/models/restaurant.rb
@@ -5,4 +5,7 @@ class Restaurant < ApplicationRecord
   has_many :orders
 
   validates :name, :delivery_tax, :city, :neighborhood, :street, :number, presence: true
+
+  has_one_attached :image
+
 end
