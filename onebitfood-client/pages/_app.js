a433b077056f2cb24072fea8eaf80699736bd105
Victor Queiroz <victoorqrz@gmail.com>
2021-12-16 11:47:38 -0300

    initial UI setup

---
+++ onebitfood-client/pages/_app.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>OneBitFood</title>
+      </Head>
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
+}
+
+export default MyApp;
