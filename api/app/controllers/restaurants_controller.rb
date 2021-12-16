f4b4cb04070798f7dcad7b50f82d499011d3da2d
Victor Queiroz <victoorqrz@gmail.com>
2021-12-15 22:24:31 -0300

    creating searching filters

--- api/app/controllers/restaurants_controller.rb
+++ api/app/controllers/restaurants_controller.rb
@@ -2,7 +2,11 @@ class RestaurantsController < ApplicationController
   before_action :set_restaurant, only: :show
 
   def index
-    @restaurants = Restaurant.all()
+    @restaurants = Restaurant.all
+    filter_by_query if params[:q]
+    filter_by_city if params[:city]
+    filter_by_category if params[:category]
+
   end
 
   def show
@@ -13,5 +17,20 @@ class RestaurantsController < ApplicationController
   def set_restaurant
     @restaurant = Restaurant.find(params[:id])
   end
+
+  def filter_by_query
+    @restaurants = @restaurants.ransack(name_or_description_cont: params[:q]).result  
+  end
+
+  def filter_by_city
+    @restaurants = @restaurants.where(city: params[:city])
+  end
   
+  def filter_by_category
+    @restaurant = @restaurants.select do |r|
+      r.category.title == params[:category]
+    end
+  end
+  
+
 end
