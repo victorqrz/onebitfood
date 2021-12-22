930564aaf5a4c15489a70470307241fee84b0459
Victor Queiroz <victoorqrz@gmail.com>
2021-12-22 11:59:36 -0300

    setting up bootstrap configs

--- onebitfood-client/pages/_app.js
+++ onebitfood-client/pages/_app.js
@@ -1,11 +1,12 @@
 import Head from "next/head";
-import "../styles/globals.css";
+import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
         <title>OneBitFood</title>
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
         <Component {...pageProps} />
--- onebitfood-client/pages/index.js
+++ onebitfood-client/pages/index.js
@@ -1,3 +1,9 @@
+import Container from "react-bootstrap/Container";
+
 export default function Home() {
-  return <></>;
+  return (
+    <Container>
+      Home page
+    </Container>
+  );
 }
