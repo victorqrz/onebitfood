310cfb6a13f8e55a02ef139dfe2e6cb9633d7a3f
Victor Queiroz <victoorqrz@gmail.com>
2022-02-17 22:44:08 -0300

    update

---
+++ onebitfood-client/components/Header/index.js
@@ -0,0 +1,7 @@
+import { Navbar } from "react-bootstrap";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Header() {
+  return <Navbar bg="white" expand="lg" className=""></Navbar>;
+}
